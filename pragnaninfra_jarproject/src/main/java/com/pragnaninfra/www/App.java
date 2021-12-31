94351b357d8f2e3618cde9df7312b450f06c5eef
jesse.m <jesse.merugolu@gmail.com>
2021-12-31 14:22:51 +0000

    adding jarproject

---
+++ pragnaninfra_jarproject/src/main/java/com/pragnaninfra/www/App.java
@@ -0,0 +1,13 @@
+package com.pragnaninfra.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Pragnaninfra; Congratulation for making till here... from jarproject.." );
+    }
+}
